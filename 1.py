38bff19039eec8b0b53c201c742f2094e44c44ce
Mawenjing <2805936956@qq.com>
2022-12-07 14:44:41 +0800

    add 文思第一个脚本

---
+++ 1.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+"""
+@Project : pythonProject1
+@File : 1.py
+@Author : wenjing
+@Date : 2022/12/1 9:30
+"""
+lst = [{'level': 19, 'star': 36, 'time': 2},
+       {'level': 20, 'star': 40, 'time': 1}]
+# lst.sort(key=lambda k: (k.get('time', 0)))
+# lst.sort(key=lambda x: x.get('time'))
+a = sorted(lst,key=lambda x:x.get('time'))
+print(a)
